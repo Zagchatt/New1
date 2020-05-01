fafc71bff47ad501edd6dc2c99e2f2267b49f4c7
zagcha <64097681+zagcha@users.noreply.github.com>
2020-05-01 05:31:55 +0300

    Update and rename Zag1.js to Zag11.js

---
+++ Zag11.js
@@ -0,0 +1,18 @@
+$('.nav-tabs').addClass('fl').css('width','100%');$
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera"><link rel="stylesheet" href="https://raw.githack.com/zagcha/v1/master/new1.css">
+</style>`).insertBefore('span.fl.fa.fa-send');
+
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<style>
+ul.nav.nav-tabs.fl li a {      color: #A1A1A1;  box-shadow: inset 0 2px 2px rgb(112, 112, 113), 0 0 8px rgb(112, 112, 113);font-size: 13px!important;padding: 6px!important;text-align: center!important;width: 100%;}
+ul.nav.nav-tabs.fl li {border-bottom: 3px;width: 33.3%;}
+.fr.co {
+    transform: rotate(60deg);
+    height: 10px;
+    width: 16px!important;
+    margin-top: 4px;
+}
+</style>`).insertBefore('body');
+$( "button.btn.btn-primary" ).css( "border-radius", "15px 15px 15px 15px" )
+$(".label-primary").css("border-radius","35px 0px 35px 0px")
+$('.uzr.fl.corner.borderg').css({'margin': '0px 0',borderRight: '1px solid #FFFFFF',borderLeft: '1px solid #FFFFFF',borderTop: '.1px solid #FFFFFF',borderBottom: '.1px solid #FFFFFF',paddingTop: '3px',borderRadius: '3px 3px 3px 3px'});
